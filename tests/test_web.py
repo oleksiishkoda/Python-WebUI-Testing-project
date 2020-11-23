1efb46db770f426dc249ebd4cbeff63c67999e2f
Oleksii Shkoda <lyoshashkoda@gmail.com>
2020-11-23 23:07:14 +0200

    Created 'config.json' and 'conftest.py'. Added new browser to functionality (firefox), all web-fixtures and validation in 'conftest.py'!

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,17 +1,9 @@
 import pytest
-from selenium.webdriver import Chrome
-from selenium.webdriver.common.keys import Keys
 
 from pages.search import DuckDuckGoSearchPage
 from pages.result import DuckDuckGoResultPage
 
 
-@pytest.fixture
-def browser():
-    driver = Chrome()
-    driver.implicitly_wait(10)
-    yield driver
-    driver.quit()
 
 def test_basic_duckduckgo_search(browser):
     # URL = 'https://www.duckduckgo.com'

405e550e02d3ccc391cc4dc76c851760d72c27a1
Oleksii Shkoda <lyoshashkoda@gmail.com>
2020-11-23 19:59:15 +0200

    Updated test_web.py with pattern Page Object, created 'pages' module with 'result.py' and 'search.py'

--- tests/test_web.py
+++ tests/test_web.py
@@ -2,23 +2,37 @@ import pytest
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.keys import Keys
 
+from pages.search import DuckDuckGoSearchPage
+from pages.result import DuckDuckGoResultPage
+
+
 @pytest.fixture
 def browser():
     driver = Chrome()
-    driver.implicitly_wait(20)
+    driver.implicitly_wait(10)
     yield driver
     driver.quit()
 
 def test_basic_duckduckgo_search(browser):
-    URL = 'https://www.duckduckgo.com'
+    # URL = 'https://www.duckduckgo.com'
+    # PHRASE = 'panda'
+    # browser.get(URL)
+    # search_input = browser.find_element_by_id('search_form_input_homepage')
+    # search_input.send_keys(PHRASE + Keys.RETURN)
+    # link_divs = browser.find_elements_by_css_selector('#links > div')
+    # assert len(link_divs) > 0
+    # xpath = f"//div[@id='links']//*[contains(text(), '{PHRASE}')]"
+    # phrase_results = browser.find_elements_by_xpath(xpath)
+    # assert len(phrase_results) > 0
+    # search_input = browser.find_element_by_id('search_form_input')
+    # assert search_input.get_attribute('value') == PHRASE
+
     PHRASE = 'panda'
-    browser.get(URL)
-    search_input = browser.find_element_by_id('search_form_input_homepage')
-    search_input.send_keys(PHRASE + Keys.RETURN)
-    link_divs = browser.find_elements_by_css_selector('#links > div')
-    assert len(link_divs) > 0
-    xpath = f"//div[@id='links']//*[contains(text(), '{PHRASE}')]"
-    phrase_results = browser.find_elements_by_xpath(xpath)
-    assert len(phrase_results) > 0
-    search_input = browser.find_element_by_id('search_form_input')
-    assert search_input.get_attribute('value') == PHRASE
+    search_page = DuckDuckGoSearchPage(browser)
+    search_page.load()
+    search_page.search(PHRASE)
+
+    result_page = DuckDuckGoResultPage(browser)
+    assert result_page.link_div_count() > 0
+    assert result_page.phrase_result_count(PHRASE) > 0
+    assert result_page.search_input_value() == PHRASE
